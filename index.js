b99a3c8961e28cbdeb0422889e57da95419cff66
Mark Calvelo <combatdstar@gmail.com>
2019-08-19 08:47:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {}; // empty Object
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object
+  var newObj = Object.new(object);
+  newObj[key] - value;
+  return newObj;
 }
